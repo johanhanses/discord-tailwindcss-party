0baa194bb19b5c044ad789b575e33c2254373f6a
Johan Hanses <johanhanses@gmail.com>
2023-02-01 08:10:19 +0100

    Channel header done

--- src/app/components/Icons.tsx
+++ src/app/components/Icons.tsx
@@ -1,21 +1,19 @@
 import { ComponentPropsWithoutRef } from 'react'
 
-export function DiscordIcon(props: ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg
-      aria-hidden="false"
-      viewBox="0 0 28 20"
-      {...props}
-    >
-      <path
-        fill="currentColor"
-        d="M23.021 1.677A21.227 21.227 0 0017.658 0c-.252.462-.483.935-.687 1.418a19.931 19.931 0 00-5.943 0C10.82.935 10.59.462 10.337.005c-1.856.32-3.659.88-5.37 1.677C1.567 6.78.65 11.754 1.111 16.652A21.504 21.504 0 007.691 20c.532-.726 1.004-1.5 1.407-2.309a13.582 13.582 0 01-2.221-1.078c.188-.137.37-.274.547-.428a15.232 15.232 0 0013.152 0c.177.148.36.291.541.428-.707.424-1.453.787-2.22 1.078.408.808.875 1.578 1.405 2.303a21.5 21.5 0 006.58-3.347h.007c.541-5.674-.922-10.6-3.868-14.97zM9.681 13.638c-1.283 0-2.34-1.193-2.34-2.644S8.37 8.35 9.68 8.35c1.308 0 2.359 1.193 2.338 2.644 0 1.451-1.036 2.644-2.339 2.644zm8.635 0c-1.283 0-2.34-1.193-2.34-2.644s1.036-2.644 2.34-2.644c1.302 0 2.36 1.193 2.338 2.644 0 1.451-1.036 2.644-2.338 2.644z"
-      />
-    </svg>
-  )
-}
+export const DiscordIcon = (props: ComponentPropsWithoutRef<'svg'>) => (
+  <svg
+    aria-hidden="false"
+    viewBox="0 0 28 20"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M23.021 1.677A21.227 21.227 0 0017.658 0c-.252.462-.483.935-.687 1.418a19.931 19.931 0 00-5.943 0C10.82.935 10.59.462 10.337.005c-1.856.32-3.659.88-5.37 1.677C1.567 6.78.65 11.754 1.111 16.652A21.504 21.504 0 007.691 20c.532-.726 1.004-1.5 1.407-2.309a13.582 13.582 0 01-2.221-1.078c.188-.137.37-.274.547-.428a15.232 15.232 0 0013.152 0c.177.148.36.291.541.428-.707.424-1.453.787-2.22 1.078.408.808.875 1.578 1.405 2.303a21.5 21.5 0 006.58-3.347h.007c.541-5.674-.922-10.6-3.868-14.97zM9.681 13.638c-1.283 0-2.34-1.193-2.34-2.644S8.37 8.35 9.68 8.35c1.308 0 2.359 1.193 2.338 2.644 0 1.451-1.036 2.644-2.339 2.644zm8.635 0c-1.283 0-2.34-1.193-2.34-2.644s1.036-2.644 2.34-2.644c1.302 0 2.36 1.193 2.338 2.644 0 1.451-1.036 2.644-2.338 2.644z"
+    />
+  </svg>
+)
 
-export function VerifiedIcon(props: ComponentPropsWithoutRef<'svg'>) {
+export const VerifiedIcon = (props: ComponentPropsWithoutRef<'svg'>) => {
   return (
     <svg
       aria-label="Verified"
@@ -32,7 +30,7 @@ export function VerifiedIcon(props: ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-export function CheckIcon(props: ComponentPropsWithoutRef<'svg'>) {
+export const CheckIcon = (props: ComponentPropsWithoutRef<'svg'>) => {
   return (
     <svg
       aria-hidden="false"
@@ -47,7 +45,7 @@ export function CheckIcon(props: ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-export function ChevronIcon(props: ComponentPropsWithoutRef<'svg'>) {
+export const ChevronIcon = (props: ComponentPropsWithoutRef<'svg'>) => {
   return (
     <svg
       viewBox="0 0 18 18"
@@ -62,7 +60,7 @@ export function ChevronIcon(props: ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-export function BookIcon(props: ComponentPropsWithoutRef<'svg'>) {
+export const BookIcon = (props: ComponentPropsWithoutRef<'svg'>) => {
   return (
     <svg
       viewBox="0 0 40 40"
@@ -78,7 +76,7 @@ export function BookIcon(props: ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-export function SpeakerphoneIcon(props: ComponentPropsWithoutRef<'svg'>) {
+export const SpeakerphoneIcon = (props: ComponentPropsWithoutRef<'svg'>) => {
   return (
     <svg
       viewBox="0 0 24 24"
@@ -92,7 +90,7 @@ export function SpeakerphoneIcon(props: ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-export function ArrowComponent(props: ComponentPropsWithoutRef<'svg'>) {
+export const ArrowComponent = (props: ComponentPropsWithoutRef<'svg'>) => {
   return (
     <svg
       viewBox="0 0 24 24"
@@ -108,7 +106,7 @@ export function ArrowComponent(props: ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-export function AddPersonIcon(props: ComponentPropsWithoutRef<'svg'>) {
+export const AddPersonIcon = (props: ComponentPropsWithoutRef<'svg'>) => {
   return (
     <svg
       viewBox="0 0 16 16"
@@ -122,7 +120,7 @@ export function AddPersonIcon(props: ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-export function HashtagIcon(props: ComponentPropsWithoutRef<'svg'>) {
+export const HashtagIcon = (props: ComponentPropsWithoutRef<'svg'>) => {
   return (
     <svg
       viewBox="0 0 24 24"
--- src/app/servers/[sid]/page.tsx
+++ src/app/servers/[sid]/page.tsx
@@ -1,10 +1,18 @@
+import { CheckIcon, ChevronIcon, VerifiedIcon } from '@/app/components/Icons'
+
 export default function ServerOne({ params }: { params: { sid: string } }) {
   return (
     <>
       <div className="flex w-60 flex-col bg-gray-800">
-        <div className="flex h-12 items-center px-3 font-title text-white shadow-sm">
-          Server {params.sid}
-        </div>
+        <button className="flex h-12 items-center px-4 font-title text-[15px] text-white shadow-sm transition hover:bg-gray-550/[0.16]">
+          <div className="relative mr-1 h-4 w-4">
+            <VerifiedIcon className="absolute h-4 w-4 text-gray-550" />
+            <CheckIcon className="absolute h-4 w-4" />
+          </div>
+          Tailwind CSS
+          <ChevronIcon className="ml-auto h-[18px] w-[18px] opacity-80" />
+        </button>
+
         <div className="flex-1 space-y-2 overflow-y-scroll p-3 font-medium text-gray-300">
           <p className="text-white">Channel (unread)</p>
           <p className="text-white">Channel (unread)</p>
